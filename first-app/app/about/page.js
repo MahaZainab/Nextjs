cf372d30b4424e5afba017aaa870e46522a39735
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-11-28 11:15:47 +0500

    Auto commit on save

--- first-app/app/about/page.js
+++ first-app/app/about/page.js
@@ -1,5 +1,7 @@
+"use client"
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+
 
 const page = () => {
     const [count, setCount] = useState(0)
