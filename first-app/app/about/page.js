a25c648ec7cb478c5f6c16a606e6b0c0c079c2d0
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2024-11-28 11:29:37 +0500

    Auto commit on save

--- first-app/app/about/page.js
+++ first-app/app/about/page.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 const page = () => {
     const [count, setCount] = useState(0)
     return (
-        <><div>I am about</div><button onClick={() => setCount(count + 1)}>click me</button></>
+        <><div>I am about {count}  </div><button onClick={() => setCount(count + 1)}>click me</button></>
     )
 }
 
